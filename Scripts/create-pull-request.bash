b8cae21d90616d0f1dc91db155971c056de6781d
olevezinho <lfilipecosta3@gmail.com>
2021-10-21 23:12:07 +0100

    test1stcommitafterchangingtoorigin/$1

--- Scripts/create-pull-request.bash
+++ Scripts/create-pull-request.bash
@@ -4,7 +4,7 @@ create_pull_request() {
 	read -p "Do you wish to create a pull request (y/n)? " ANSWER3
 	if [ $ANSWER3 == 'y' ] || [ $ANSWER3 == 'Y' ];
 	then
-	    gh pr create --base $1 #--head $2
+	    gh pr create --base origin/$1 #--head $2
             exit 0
 	else
 	    echo "No PR was requested!"
