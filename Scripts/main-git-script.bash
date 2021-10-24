745d7ad0fbe39ab9041c7e1c2afe41b27999af03
Luis Costa Brochado <37152156+olevezinho@users.noreply.github.com>
2021-10-24 23:07:05 +0100

    Update main-git-script.bash

--- Scripts/main-git-script.bash
+++ Scripts/main-git-script.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. /full-filepath-to/basic-git-operations.bash
+. ~/Documents/source/repos/basic-git-operations.bash
 
 # Execute the flow
 main() {
@@ -8,7 +8,7 @@ main() {
     gh auth login
     
     # read name of the default branch
-    MAIN_BRANCH=`git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/@@'` # or git rev-parse --abbrev-ref origin/HEAD
+    MAIN_BRANCH=`git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'` # or git rev-parse --abbrev-ref origin/HEAD
     
     git status
     if [ $? -eq 0 ];
@@ -20,16 +20,28 @@ main() {
         then
         # If the branch names don't match, push changes. Then do a checkout to main branch, perform pull. 
         # After that, checkout to previous branch, and merge main branch into current branch
-            basic_git_operations $ACTIVE_BRANCH
+            read -p "Do you want to commit and push your changes (y/n)? " ANSWER
+            if [ $ANSWER == 'y' ];
+            then
+                echo "You're about to push your changes"
+                git add .
+                read -p "What's the message you desire to type? " MESSAGE
+                git commit -m "$MESSAGE"
+                git push -u origin $ACTIVE_BRANCH
+            else
+                    echo "Hope to see you again!"
+            fi
+            Flag='false'
             git checkout $MAIN_BRANCH
             git pull
             git checkout $ACTIVE_BRANCH
             git merge $MAIN_BRANCH
-            basic_git_operations $ACTIVE_BRANCH
+            basic_git_operations $Flag $ACTIVE_BRANCH
             exit 0
         else
         # else, everything is ok
-            basic_git_operations $ACTIVE_BRANCH 
+            Flag='true'
+            basic_git_operations $Flag $MAIN_BRANCH
             exit 0
         fi
     else
