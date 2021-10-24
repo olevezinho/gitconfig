4f0c33def66a61e4bc68d35f1f7eaa6c06d81f93
Luis Costa Brochado <37152156+olevezinho@users.noreply.github.com>
2021-10-24 11:25:00 +0100

    Update main-git-script.bash
    replacing filepaths

--- Scripts/main-git-script.bash
+++ Scripts/main-git-script.bash
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-. /home/merkle-filipe/source/repos/gitconfig/Scripts/basic-git-operations.bash
-. /home/merkle-filipe/source/repos/gitconfig/Scripts/create-pull-request.bash
+. /full-filepath-to/basic-git-operations.bash
 
 # Execute the flow
-main() { 
-    read -p "What is the name of the main branch? " MAIN_BRANCH
+main() {
+    # authenticate against github api
+    gh auth login
+    
+    # read name of the default branch
+    MAIN_BRANCH=`git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/@@'` # or git rev-parse --abbrev-ref origin/HEAD
+    
     git status
     if [ $? -eq 0 ];
     then
         # Resume the flow
-        ACTIVE_BRANCH=`git status | awk '{print $3}' | awk 'FNR == 1'`
+        ACTIVE_BRANCH=`git status | awk '{print $3}' | awk 'FNR == 1'` # or git rev-parse --abbrev-ref HEAD
         # Use the name of the branch
         if [[ "$ACTIVE_BRANCH" != "$MAIN_BRANCH" ]];
         then
-        # if they don't match, do a checkout to main branch, perform pull, merge previous branch into main branch
+        # If the branch names don't match, push changes. Then do a checkout to main branch, perform pull. 
+        # After that, checkout to previous branch, and merge main branch into current branch
+            basic_git_operations $ACTIVE_BRANCH
             git checkout $MAIN_BRANCH
             git pull
-	    git checkout $ACTIVE_BRANCH
+            git checkout $ACTIVE_BRANCH
             git merge $MAIN_BRANCH
-            basic_git_operations
-	    create_pull_request "$MAIN_BRANCH" "$ACTIVE_BRANCH"
+            basic_git_operations $ACTIVE_BRANCH
             exit 0
-        else 
+        else
         # else, everything is ok
-            basic_git_operations
-	    exit 0
+            basic_git_operations $ACTIVE_BRANCH 
+            exit 0
         fi
     else
         echo "You've got no changes!"
